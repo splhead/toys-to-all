d66fb6e88588fc9268d8ce980b5ed709420dc59e
Silas P Ladislau <splhead@gmail.com>
2021-12-26 22:00:38 -0400

    checkpoint

--- toysToAll/ToyViewController.swift
+++ toysToAll/ToyViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ToyViewController: UIViewController {
 
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var segmentedControlState: UISegmentedControl!
+    @IBOutlet weak var textFieldDonorName: UITextField!
+    @IBOutlet weak var textFieldPhone: UITextField!
+    @IBOutlet weak var textViewAddress: UITextView!
+    @IBOutlet weak var buttonSave: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func save(_ sender: UIButton) {
+    }
 }
 
